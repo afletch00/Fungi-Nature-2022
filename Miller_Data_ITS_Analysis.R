7ffc3da0bc5869662f4bcb1e85b5cad656a61af6
afletch00 <58954813+afletch00@users.noreply.github.com>
2022-06-08 12:22:17 -0400

    Update Miller_Data_ITS_Analysis.R
    Code clean-up and comments inserted

--- Miller_Data_ITS_Analysis.R
+++ Miller_Data_ITS_Analysis.R
@@ -1,3 +1,6 @@
+# Libraries used for microbiome analysis- vegan, phyloseq, microbiome, microbiomeSeq, microbiomeutilities
+
+# Read in sample files made with QIIME
 
 sample_info<- read.table("meta.all.tsv", header=T, row.names=1,
                          check.names=F, sep="\t")
@@ -11,9 +14,11 @@ count_tab <- read.table("QIIME_all_OTU_tab.tsv", header=T,
 ps_allQ <- phyloseq(otu_table(count_tab, taxa_are_rows=TRUE), sample_data(sample_info), 
                          tax_table(tax_tab))
 
+# Check
 sample_sums(ps_allQ)
 tax_table(ps_allQ)
 
+# Remove taxa not assigned at Domain level (Kingdom)
 
 rank_names(ps_allQ)
 ps_taxa_filt <- subset_taxa(ps_allQ, !is.na(domain) & !domain %in% c("", "uncharacterized"))
@@ -32,29 +37,31 @@ filterDomain = c("unidentified")
 ps_filtDom = subset_taxa(ps_taxa_filt, !domain %in% filterDomain)
 prevdf1 = subset(prevdf, domain %in% get_taxa_unique(ps_filtDom, "domain"))
 
-plyr::ddply(prevdf1, "phylum", function(df1){cbind(mean(df1$Prevalence),sum(df1$Prevalence))})
-filterPhyla = c("unidentified")
-ps_filtPhy = subset_taxa(ps_filtDom, !phylum %in% filterPhyla)
-prevdf2 = subset(prevdf1, phylum %in% get_taxa_unique(ps_filtPhy, "phylum"))
-
-keepTaxa = rownames(prevdf2)
+keepTaxa = rownames(prevdf1)
 ps_filt_2 = prune_taxa(keepTaxa, ps_taxa_filt)
 
 # filter taxa (if any) with 0 counts
 ps_filt_2 = prune_taxa(taxa_sums(ps_filt_2) > 0, ps_filt_2)
 ## filter out single OTUs
 ps_filt_2 = filter_taxa(ps_filt_2, function (x) {sum(x > 0) > 1}, prune=TRUE)
+
+# Check
 sample_data(ps_filt_2)
 tax_table(ps_filt_2)
+
 sample_data(ps_filt_2)$sample_type <- as.factor(sample_data(ps_filt_2)$sample_type)
 
-##################### Subset samples ###########################
+# Make seperate phyloseq objects for gut and human samples
 
 ps_subG = subset_samples(ps_filt_2, sample_type %in% c("Norm. Gut", "PDAC Gut"))
 ps_subT = subset_samples(ps_filt_2, sample_type %in% c("Norm. Panc.", "PDAC Panc."))
 
+# Aggregate to top 10 Genera
+
 ps.gen10T <- aggregate_top_taxa2(ps_subT, "genus", top = 10) 
 ps.gen10G <- aggregate_top_taxa2(ps_subG, "genus", top = 10)
+
+# Plot read counts
 
 plot.composition.COuntAbun <- plot_composition(ps.gen10T, group_by = "sample_type", otu.sort = "abundance",
                                                sample.sort = "Malassezia", 
@@ -69,39 +76,33 @@ plot.composition.COuntAbun <- plot_composition(ps.gen10T, group_by = "sample_typ
 #plot.composition.COuntAbun$data$Group <- factor(plot.composition.COuntAbun$data$Group, levels = desired_order)
 plot.composition.COuntAbun
 
-### Malassezia Only ###
+# Subset gut and pancreas samples to Domain and Malassezia only and plot
 
-ps.mal <- subset_taxa(ps.gen10G, genus == "Malassezia")
-#ps.mal <- aggregate_taxa(ps.mal, "genus")
+ps.dom <- aggregate_taxa(ps_subG, "domain", verbose = FALSE) # ps_subT
+ps.mal <- subset_taxa(ps.gen10G, genus == "Malassezia") # ps_sub10T
 sample_sums(ps.mal)
-#ps.mal = prune_samples(sample_sums(ps.mal) > 0, ps.mal)
-ps.dom <- aggregate_taxa(ps_subG, "domain", verbose = FALSE)
-#ps.dom = prune_samples(sample_sums(ps.dom) > 0, ps.dom)
-meltsub <- psmelt(ps.dom)
+meltsub <- psmelt(ps.dom) # psmelt(ps.mal)
 meltsub
 
 psmelt = ggplot(meltsub, aes(x = sample_type, y = Abundance)) +  ylim(0,2000) +
   geom_boxplot(outlier.shape  = NA, size = 1.5) +
-  #scale_y_continuous(expand = expansion(mult = c(0.1, .15))) + 
   stat_compare_means(label.y = Inf, vjust = 1.5, size = 7, label.x.npc = 0.135) +
-                     #method.args = list(alternative = "less")) +
   geom_jitter(aes(color=sample_type, fill=sample_type), shape=21, size = 7,
               color = "black", position = position_jitterdodge(1))+
-  #scale_fill_manual(values=c( "#FFCD00FF", "#FFCD00FF")) +
-  scale_fill_manual(values=c("green3", "red2")) +
+  #scale_fill_manual(values=c( "#FFCD00FF", "#FFCD00FF")) + # For Malassezia
+  scale_fill_manual(values=c("green3", "red2")) + # blue and orange for pancreas
   theme_bw(base_size = 27) +
   labs(x = "", y = "Read Counts\n") +
   theme(legend.position = "none") +
   facet_wrap(~ OTU, scales = "free") 
 psmelt
 
-################# Relative Abundance ################################
+# Transform to relative abundance and plot
 
 ps.gen.relT <- microbiome::transform(ps.gen10T, "compositional")
 ps.gen.relG <- microbiome::transform(ps.gen10G, "compositional")
 
-
-plot.composition.relAbunT <- plot_composition(ps.gen.relT, group_by = "sample_type", #otu.sort = "abundance",
+plot.composition.relAbunT <- plot_composition(ps.gen.relT, group_by = "sample_type", #otu.sort = "abundance", 
                                              otu.sort = c("Candida", "Malassezia", "Cystobasidium", "Aureobasidium",
                                                           "Aspergillus", "Tilletia", "Ustilago", "Exophiala", "Sclerotinia","Unknown","Other"),
                                              sample.sort = "Malassezia", 
@@ -110,10 +111,8 @@ plot.composition.relAbunT <- plot_composition(ps.gen.relT, group_by = "sample_ty
                                 "plum3" ,"#00AF66FF" ,"steelblue1","tomato","grey37", "grey87")) +
   theme_bw(base_size = 17) + 
   geom_bar(colour = "black", stat = "identity") +
-  #scale_fill_startrek_adaptive() + theme_bw(base_size = 17) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) + 
   labs(x = "", y = "Read Counts\n") +
-  #guides(fill=guide_legend(label.theme = element_text(face = "italic")), size = 15) +
   theme(legend.text = element_text(colour="black", size = 15, face = "italic")) +
   guides(fill=guide_legend(title="Genera"))
 plot.composition.relAbunT
@@ -128,66 +127,41 @@ plot.composition.relAbunG <- plot_composition(ps.gen.relG, group_by = "sample_ty
                                 "cyan3" ,"plum" ,"royalblue3","tomato","grey37", "grey87")) +
   theme_bw(base_size = 17) + 
   geom_bar(colour = "black", stat = "identity") +
-  #scale_fill_startrek_adaptive() + theme_bw(base_size = 17) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) + 
   labs(x = "", y = "Read Counts\n") +
-  #guides(fill=guide_legend(label.theme = element_text(face = "italic")), size = 15) +
   theme(legend.text = element_text(colour="black", size = 15, face = "italic")) +
   guides(fill=guide_legend(title="Genera"))
 plot.composition.relAbunG
 
+# Make Alpha diveristy plots with wilcoxon derived p-values
+# Remove samples with 0 ASVs
 
-ps.melt.gen <- psmelt(ps.gen.rel) 
-
-ps.gen.rel2 <- subset_taxa(ps.gen.rel, genus == "Malassezia")
-ps.melt.gen <- psmelt(ps.gen.rel2) 
-
-psmelt = ggplot(ps.melt.gen, aes(x = sample_type, y = Abundance)) +  #ylim(0,1) +
-  geom_boxplot(outlier.shape  = NA) +
-  scale_y_continuous(expand = expansion(mult = c(0.1, .15))) + 
-  stat_compare_means(label.y = Inf, vjust = 1.5, size = 3) +
-  geom_jitter(aes(color = OTU), height = 0, width = 0, size = 3) +
-  scale_color_futurama_adaptive() +
-  theme_grey(base_size = 10) +
-  labs(x = "", y = "Relative Abundance\n") +
-  #ggtitle("Malassezia: Relative Abundance") +
-  facet_wrap(~ OTU, scales = "free", ncol = 4) 
-psmelt
-
-################## Alpha DIversity ########
-
-### Run if any samples have 0 ASVs ###
 ps_sub0T = prune_samples(sample_sums(ps_subT) > 0, ps_subT)
 ps_sub0G = prune_samples(sample_sums(ps_subG) > 0, ps_subG)
 
-p1 = plot_richness(ps_sub0G, x="sample_type", measures=c("Observed", "ACE", "Chao1", "Shannon", "Simpson" )) +
+p1 = plot_richness(ps_sub0G, x="sample_type", measures=c("Observed", "ACE", "Chao1", "Shannon", "Simpson" )) + #ps-sub0T
   geom_boxplot(size = 1, color="black") +
   theme(text = element_text(size = 21, color = "black")) + 
   theme_bw(base_size = 19) +
   theme(axis.text.x = element_text(angle =45, vjust = 1, hjust=1)) +
   scale_y_continuous(expand = expansion(mult = 0.15)) +
-  #ggtitle("Alpha Diversity Measure") +
   stat_compare_means(method = "wilcox", label.y = Inf, vjust = 1.5, label = "p", size = 5) 
-                     #method.args = list(alternative = "less"))
-#p1$data$Group <- factor(p1$data$Group, levels = desired_order)
 p1 + labs(x = "", y = "Alpha Diversity Measure") 
 
+# Make ordination plot (PCoA) using Bray-Curtis distance matrix and PERMANOVA derived p-values
 
-### Bray on raw ####
-
-otu_r <- abundances(ps_sub0G)
-meta_r <- meta(ps_sub0G)
+otu_r <- abundances(ps_sub0G) # ps-sub0T
+meta_r <- meta(ps_sub0G) # ps-sub0T
 permanova <- adonis(t(otu_r) ~ sample_type,
                     data = meta_r, permutations=999, method = "bray") 
 permanova
 
-ps_nmdsB <- ordinate(physeq = ps_sub0G, method = "PCoA", distance = "bray")
-plot_ordination(physeq = ps_sub0G, ordination = ps_nmdsB,
-                color = "sample_type",title = "p = 0.47") + theme_bw()+
-  #scale_color_lancet() +
-  scale_colour_manual(values=c("Norm. Gut"="green3", "PDAC Gut"="red2")) +
+ps_nmdsB <- ordinate(physeq = ps_sub0G, method = "PCoA", distance = "bray") # ps-sub0T
+
+plot_ordination(physeq = ps_sub0G, ordination = ps_nmdsB, # ps-sub0T
+                color = "sample_type",title = "p = 0.47") + theme_bw()+   # Use p-value derived from adonis for title
+  scale_colour_manual(values=c("Norm. Gut"="green3", "PDAC Gut"="red2")) + # blue and orange for pancreas tissue
   geom_point(aes(color = sample_type), size = 5) + 
-  #geom_point(size = 4) + 
   theme_classic(base_size = 23) + 
   theme(legend.title = element_blank()) +
   theme(plot.title = element_text(vjust =  -7, hjust = 1.0, size = 23)) +
